6c5f3f653e501fd12218c89b176092b2b1157f45
Darren Li <tm455340@gmail.com>
2023-06-18 22:30:46 +0800

    add swiper slide

--- src/components/VideoPlayer.vue
+++ src/components/VideoPlayer.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="w-full h-full relative flex justify-center">
     <video-player
+      id="videoPlayer"
+      ref="RefVideoPlayer"
       :src="videoInfo.play_url"
       :poster="videoInfo.cover"
       loop
-      controls
       fill
+      @touchmove.prevent
+      @mounted="handleMounted"
     ></video-player>
     <div class="absolute left-2 bottom-8 font-medium text-slate-50">
       {{ videoInfo.title }}
@@ -14,13 +17,28 @@
 </template>
 
 <script setup>
+import { shallowRef, watch } from "vue";
+import { useSwiperSlide } from "swiper/vue";
 import { VideoPlayer } from "@videojs-player/vue";
 import "video.js/dist/video-js.css";
-defineProps({
+const props = defineProps({
   videoInfo: {
     type: Object,
     required: true,
   },
+});
+const player = shallowRef();
+const swiperSlide = useSwiperSlide();
+const handleMounted = (payload) => {
+  player.value = payload.player;
+};
+watch(swiperSlide, async (val) => {
+  if (val.isActive) {
+    player.value?.play();
+  } else {
+    player.value?.pause();
+    player.value?.currentTime(0);
+  }
 });
 </script>
 
--- src/views/forYou.vue
+++ src/views/forYou.vue
@@ -1,13 +1,14 @@
 <template>
-  <VideoPlayer
-    v-for="video in videoList"
-    :key="video.cover"
-    :videoInfo="video"
-  ></VideoPlayer>
+  <Swiper class="w-full h-full" :direction="'vertical'">
+    <SwiperSlide v-for="video in videoList" :key="video.cover">
+      <VideoPlayer :videoInfo="video"></VideoPlayer>
+    </SwiperSlide>
+  </Swiper>
 </template>
 
 <script setup>
 import { reactive, defineComponent } from "vue";
+import { Swiper, SwiperSlide } from "swiper/vue";
 import VideoPlayer from "@/components/VideoPlayer.vue";
 defineComponent({ VideoPlayer });
 const videoList = reactive([]);
